9b5f55f79ae8e7c8d9eb98f9e8ec136e5ff51fc6
EdrilanBerisha <EdrilanBerisha@users.noreply.github.com>
2026-05-29 02:50:12 +0000

    Deploying to gh-pages from @ EdrilanBerisha/ui5-webcomponents@18c52b8c6d0c9f50b8082ddf932ae505cf58ddfe 🚀

---
+++ nightly/assets/ListItemStandardExpandableTextTemplate-BCGO07fE.js
@@ -0,0 +1,1 @@
+import{j as x,az as m}from"./bundle.esm-CoW6swvV.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.5",
     isNext: false,
-    buildTime: 1779936100,
+    buildTime: 1780022683,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
