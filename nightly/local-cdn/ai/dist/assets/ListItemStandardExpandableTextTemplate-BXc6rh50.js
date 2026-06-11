2cf6b8d55f30b4d7d5674766d7c00123317808a8
EdrilanBerisha <EdrilanBerisha@users.noreply.github.com>
2026-06-11 03:31:30 +0000

    Deploying to gh-pages from @ EdrilanBerisha/ui5-webcomponents@18c52b8c6d0c9f50b8082ddf932ae505cf58ddfe 🚀

---
+++ nightly/local-cdn/ai/dist/assets/ListItemStandardExpandableTextTemplate-BXc6rh50.js
@@ -0,0 +1,1 @@
+import{a as x,E as m}from"./bundle.esm-Dc7TFQhd.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
