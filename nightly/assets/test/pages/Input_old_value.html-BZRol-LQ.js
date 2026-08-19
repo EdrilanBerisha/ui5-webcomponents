ffc0839c95063ce7473eab629a70f4905ee6ea7d
EdrilanBerisha <EdrilanBerisha@users.noreply.github.com>
2026-08-19 01:00:52 +0000

    Deploying to gh-pages from @ EdrilanBerisha/ui5-webcomponents@18c52b8c6d0c9f50b8082ddf932ae505cf58ddfe 🚀

--- nightly/assets/test/pages/Input_old_value.html-BZRol-LQ.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-mrzw0UuF.js";let e="";inp1.addEventListener("input",t=>{t.target.value.length<=3&&(e=t.target.value),inp1.value=e,inp2.value=e});inp2.addEventListener("input",t=>{t.target.value.length<=3&&(e=t.target.value),inp1.value=e,inp2.value=e});
