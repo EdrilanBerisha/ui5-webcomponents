ed4b24a2ed2d49fa628f3520aaa309d063157397
EdrilanBerisha <EdrilanBerisha@users.noreply.github.com>
2026-08-20 01:00:46 +0000

    Deploying to gh-pages from @ EdrilanBerisha/ui5-webcomponents@18c52b8c6d0c9f50b8082ddf932ae505cf58ddfe 🚀

---
+++ nightly/assets/test/pages/Input_old_value.html-BoCb_hZN.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-CjC5-evc.js";let e="";inp1.addEventListener("input",t=>{t.target.value.length<=3&&(e=t.target.value),inp1.value=e,inp2.value=e});inp2.addEventListener("input",t=>{t.target.value.length<=3&&(e=t.target.value),inp1.value=e,inp2.value=e});
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.5",
     isNext: false,
-    buildTime: 1787100929,
+    buildTime: 1787187323,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
