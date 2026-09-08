4bc5eba099a799d98e8a4dc49b0f7d6f526ebcaa
EdrilanBerisha <EdrilanBerisha@users.noreply.github.com>
2026-09-08 02:44:10 +0000

    Deploying to gh-pages from @ EdrilanBerisha/ui5-webcomponents@18c52b8c6d0c9f50b8082ddf932ae505cf58ddfe 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.5",
     isNext: false,
-    buildTime: 1788748015,
+    buildTime: 1788835198,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
