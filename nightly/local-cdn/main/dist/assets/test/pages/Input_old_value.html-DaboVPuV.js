88f96ddfa1b535009416f98e6d4cbb327e715f64
EdrilanBerisha <EdrilanBerisha@users.noreply.github.com>
2026-09-12 02:50:05 +0000

    Deploying to gh-pages from @ EdrilanBerisha/ui5-webcomponents@18c52b8c6d0c9f50b8082ddf932ae505cf58ddfe 🚀

--- nightly/local-cdn/main/dist/assets/test/pages/Input_old_value.html-DaboVPuV.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-eHvIQx6F.js";let e="";inp1.addEventListener("input",t=>{t.target.value.length<=3&&(e=t.target.value),inp1.value=e,inp2.value=e});inp2.addEventListener("input",t=>{t.target.value.length<=3&&(e=t.target.value),inp1.value=e,inp2.value=e});
